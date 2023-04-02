bdd52b7e2712a78410f630ee1e809d8ef2447cfa
shirocola <kitpivatpiromgit@gmail.com>
2023-04-03 01:50:26 +0700

    import all components

---
+++ Skill-Kamp-FE/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import PromoCodeBar from './components/Promocode'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Footer from './components/Footer'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <PromoCodeBar />
+    <Header />
+    <App />
+    <Categories />
+    <Footer />
+  </React.StrictMode>,
+)
